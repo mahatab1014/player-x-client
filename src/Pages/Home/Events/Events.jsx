6f4a145b02f5066fb75ddc74b5c6f2de8ddc5ec3
DeveloperMahTab <mahtabhossain1014@gmail.com>
2023-10-07 17:09:53 +0600

    events countdown timer

---
+++ src/Pages/Home/Events/Events.jsx
@@ -0,0 +1,94 @@
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+const Events = () => {
+  const minuteSeconds = 60;
+  const hourSeconds = 3600;
+  const daySeconds = 86400;
+  const timerProps = {
+    isPlaying: true,
+    size: 100,
+    strokeWidth: 8,
+  };
+  const renderTime = (dimension, time) => {
+    return (
+      <div className="time-wrapper">
+        <div className="time">{time}</div>
+        <div>{dimension}</div>
+      </div>
+    );
+  };
+
+  const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
+  const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
+  const getTimeHours = (time) => ((time % daySeconds) / hourSeconds) | 0;
+  const getTimeDays = (time) => (time / daySeconds) | 0;
+  const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = stratTime + 243248; // use UNIX timestamp in seconds
+
+  const remainingTime = endTime - stratTime;
+  const days = Math.ceil(remainingTime / daySeconds);
+  const daysDuration = days * daySeconds;
+
+  return (
+    <div className="flex justify-center gap-5 text-center">
+      <CountdownCircleTimer
+        {...timerProps}
+        colors="#ffff00"
+        duration={daysDuration}
+        initialRemainingTime={remainingTime}
+      >
+        {({ elapsedTime, color }) => (
+          <span style={{ color }}>
+            {renderTime("days", getTimeDays(daysDuration - elapsedTime))}
+          </span>
+        )}
+      </CountdownCircleTimer>
+      <CountdownCircleTimer
+        {...timerProps}
+        colors="#ffff00"
+        duration={daySeconds}
+        initialRemainingTime={remainingTime % daySeconds}
+        onComplete={(totalElapsedTime) => ({
+          shouldRepeat: remainingTime - totalElapsedTime > hourSeconds,
+        })}
+      >
+        {({ elapsedTime, color }) => (
+          <span style={{ color }}>
+            {renderTime("hours", getTimeHours(daySeconds - elapsedTime))}
+          </span>
+        )}
+      </CountdownCircleTimer>
+      <CountdownCircleTimer
+        {...timerProps}
+        colors="#ffff00"
+        duration={hourSeconds}
+        initialRemainingTime={remainingTime % hourSeconds}
+        onComplete={(totalElapsedTime) => ({
+          shouldRepeat: remainingTime - totalElapsedTime > minuteSeconds,
+        })}
+      >
+        {({ elapsedTime, color }) => (
+          <span style={{ color }}>
+            {renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))}
+          </span>
+        )}
+      </CountdownCircleTimer>
+      <CountdownCircleTimer
+        {...timerProps}
+        colors="#ffff00"
+        duration={minuteSeconds}
+        initialRemainingTime={remainingTime % minuteSeconds}
+        onComplete={(totalElapsedTime) => ({
+          shouldRepeat: remainingTime - totalElapsedTime > 0,
+        })}
+      >
+        {({ elapsedTime, color }) => (
+          <span style={{ color }}>
+            {renderTime("seconds", getTimeSeconds(elapsedTime))}
+          </span>
+        )}
+      </CountdownCircleTimer>
+    </div>
+  );
+};
+
+export default Events;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import FeaturedCollection from "../../Components/FeaturedCollection/FeaturedCollection";
 import ServiceGrid from "../../Components/Services/ServiceGrid";
+import Events from "./Events/Events";
 import Slider from "./Slider/Slider";
 
 const Home = () => {
@@ -23,6 +24,36 @@ const Home = () => {
             </p>
           </div>
           <ServiceGrid />
+        </div>
+      </section>
+
+      <section className="py-20 bg-base-200">
+        <div className="container mx-auto">
+          <div className="section-title text-center">
+            <h2 className="text-5xl font-semibold uppercase text-white">
+              Upcoming<span className="text-primary"> Events</span>
+            </h2>
+            <p className="md:max-w-3xl md:m-auto pt-2 pb-5">
+              Stay ahead of the gaming curve with our 'Events Countdown.' Get
+              the inside track on upcoming gaming tournaments, releases, and
+              conventions. Watch the seconds tick away as you anticipate the
+              next big gaming event. Click on an event to learn more and mark
+              your calendar for gaming greatness!
+            </p>
+          </div>
+          <Events />
+          <div className="text-center pt-10 space-y-3">
+            <h4 className="text-2xl">Subscribe for next events update!</h4>
+            <div className="join">
+              <input
+                className="input input-bordered join-item"
+                placeholder="Email"
+              />
+              <button className="btn bg-secondary hover:bg-secondary text-black join-item rounded-r-full">
+                Subscribe
+              </button>
+            </div>
+          </div>
         </div>
       </section>
 
