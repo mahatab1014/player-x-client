024bf1042780293bf855b868c6497c2595d70db9
DeveloperMahTab <mahtabhossain1014@gmail.com>
2023-10-09 19:30:51 +0600

    custom cursor added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,9 +2,11 @@ import { Outlet, useNavigation } from "react-router-dom";
 import MainNavbar from "../Components/Navbar/MainNavbar";
 import Footer from "../Components/Footer/Footer";
 import Skeleton from "../Components/LoadingAnimation/Skeleton";
+import AnimatedCursor from "react-animated-cursor";
 
 const MainLayout = () => {
   const navigation = useNavigation();
+
   return (
     <>
       <header className="shadow-xl shadow-[#8e87e629]">
@@ -16,6 +18,19 @@ const MainLayout = () => {
       <footer>
         <Footer />
       </footer>
+
+      <AnimatedCursor
+        innerSize={12}
+        outerSize={38}
+        outerScale={1.3}
+        outerAlpha={0.2}
+        showSystemCursor={true}
+        color="255, 195, 15"
+        outerStyle={{
+          border: "3px solid #ffc30f",
+          background: "transparent"
+        }}
+      />
     </>
   );
 };
